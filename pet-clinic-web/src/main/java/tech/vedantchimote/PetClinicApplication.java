67351d61c549c6a3ff862b39ddc629b58f5231b3
Vedant Chimote <vedantc.code@gmail.com>
2023-09-06 15:29:09 +0530

    Created Docker Image for both modules (data and web) and pushed to DockerHub: Closes #11

--- pet-clinic-web/src/main/java/tech/vedantchimote/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/tech/vedantchimote/PetClinicApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PetClinicApplication {
 
+  //Test endpoint
   @GetMapping("/welcome")
   public String getMessage() {
     return "Hello World!";
