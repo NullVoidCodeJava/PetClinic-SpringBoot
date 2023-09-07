334908120153a06b1a5be216e85370933849490d
vedantchimote <vedantc.code@gmail.com>
2023-09-07 22:50:30 +0530

    Created Vet Index Page and Controller : Closes #7

---
+++ pet-clinic-web/src/main/java/tech/vedantchimote/controllers/VetController.java
@@ -0,0 +1,17 @@
+/**
+ * Created By Vedant Chimote Date : 07-09-2023 Time : 22:47 Project-Name : ownpetclinic
+ */
+
+package tech.vedantchimote.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+  @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+  public String vets() {
+    return "vets/index";
+  }
+}
