a5dd3098c565708e1fa7d42c55f3670000e1d48b
vedantchimote <vedantc.code@gmail.com>
2023-10-13 17:34:55 +0530

    :lipstick: Add VetService dependency injection in VetController and modify the vets method to pass the vet list to the view. Closes #21

--- pet-clinic-web/src/main/java/tech/vedantchimote/controllers/VetController.java
+++ pet-clinic-web/src/main/java/tech/vedantchimote/controllers/VetController.java
@@ -4,15 +4,32 @@
 
 package tech.vedantchimote.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tech.vedantchimote.services.VetService;
 
 
 @Controller
 public class VetController {
 
+  //cant set final as in some test method the default constructor is used
+  private VetService vetService;
+  ;
+
+  @Autowired
+  public VetController(VetService vetService) {
+    this.vetService = vetService;
+  }
+
+  public VetController() {
+  }
+
   @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-  public String vets() {
+  public String vets(Model model) {
+
+    model.addAttribute("vets", this.vetService.findAll());
     return "vets/index";
   }
 }
